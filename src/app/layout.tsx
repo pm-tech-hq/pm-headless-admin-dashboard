1532f5174c0dce764eab6cc8d389dce672194fda
pm-tech-hq <x@pmoluno.com>
2026-03-14 18:12:44 -0600

    chore: remove unused Poppins font files and update layout to reflect changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,15 +7,10 @@ import { SessionProvider } from '@/components/auth/SessionProvider';
 
 const poppins = localFont({
   src: [
-    { path: '../../public/fonts/poppins/Poppins-Thin.ttf', weight: '100', style: 'normal' },
-    { path: '../../public/fonts/poppins/Poppins-ExtraLight.ttf', weight: '200', style: 'normal' },
-    { path: '../../public/fonts/poppins/Poppins-Light.ttf', weight: '300', style: 'normal' },
     { path: '../../public/fonts/poppins/Poppins-Regular.ttf', weight: '400', style: 'normal' },
     { path: '../../public/fonts/poppins/Poppins-Medium.ttf', weight: '500', style: 'normal' },
     { path: '../../public/fonts/poppins/Poppins-SemiBold.ttf', weight: '600', style: 'normal' },
     { path: '../../public/fonts/poppins/Poppins-Bold.ttf', weight: '700', style: 'normal' },
-    { path: '../../public/fonts/poppins/Poppins-ExtraBold.ttf', weight: '800', style: 'normal' },
-    { path: '../../public/fonts/poppins/Poppins-Black.ttf', weight: '900', style: 'normal' },
   ],
   variable: '--font-poppins',
 });
